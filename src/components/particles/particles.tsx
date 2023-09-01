bb63d4e2ca94bfb2aea3fabdbc5ffc2005b0e5ce
esteban <jhojan.e.molina@novaip.com.co>
2023-09-01 02:52:44 -0500

    se  continuo la pagina

--- src/components/particles/particles.tsx
+++ src/components/particles/particles.tsx
@@ -6,77 +6,6 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 //import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 
-const particlesOptions = (props:any) => {
-    return {
-        background: {
-            color: {
-                value: "#000",
-            },
-        },
-        fpsLimit: 60,
-        interactivity: {
-            events: {
-                onClick: {
-                    enable: true,
-                    mode: "push",
-                },
-                onHover: {
-                    enable: true,
-                    mode: "repulse",
-                },
-                resize: true,
-            },
-            modes: {
-                push: {
-                    quantity: 4,
-                },
-                repulse: {
-                    distance: 200,
-                    duration: 0.4,
-                },
-            },
-        },
-        particles: {
-            color: {
-                value: "#ffffff",
-            },
-            links: {
-                color: "#ffffff",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-            },
-            move: {
-                direction: "none",
-                enable: true,
-                outModes: {
-                    default: "bounce",
-                },
-                random: false,
-                speed: 1,
-                straight: false,
-            },
-            number: {
-                density: {
-                    enable: true,
-                    area: 600,
-                },
-                value: 50,
-            },
-            opacity: {
-                value: 0.5,
-            },
-            shape: {
-                type: props.shapes,
-            },
-            size: {
-                value: { min: 1, max: 5 },
-            },
-        },
-        detectRetina: true,
-    }
-};
 
 const ParticleBackground = (props:any) => {
     const particlesInit = useCallback(async (engine: Engine) => {
@@ -97,7 +26,75 @@ const ParticleBackground = (props:any) => {
             id="tsparticles"
             init={particlesInit}
             loaded={particlesLoaded}
-            options={particlesOptions({shapes:props.shapes})}
+            options={{
+                background: {
+                    color: {
+                        value: "#000",
+                    },
+                },
+                fpsLimit: 60,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 1,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 600,
+                        },
+                        value: 50,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: props.shapes,
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
         />
     );
 };
